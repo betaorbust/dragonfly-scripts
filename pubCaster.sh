2ff37a1ee78c32eadee20ac344cf45e9cf4641d1
Jacques Favreau <betaorbust@users.noreply.github.com>
2017-11-23 10:59:34 -0800

    Make pubCaster exclude .git and bin from local caster install.

--- pubCaster.sh
+++ pubCaster.sh
@@ -1,4 +1,4 @@
 while inotifywait -r -e modify,create,delete /home/jfavreau/code/caster; do
-    rsync -avz /home/jfavreau/code/caster/caster/ /home/jfavreau/code/dragonfly-scripts/caster/
-    rsync -avz /home/jfavreau/code/caster/_caster.py /home/jfavreau/code/dragonfly-scripts/_caster.py
+    rsync -av --exclude '.git' --exclude 'bin' /home/jfavreau/code/caster/caster/ /home/jfavreau/code/dragonfly-scripts/caster/
+    rsync -av /home/jfavreau/code/caster/_caster.py /home/jfavreau/code/dragonfly-scripts/_caster.py
 done
