44aa93b45f0a89bff29eb2ea7c71fd47c713dd85
Jacques Favreau <Jacques@lgml-jfavreau.corp.netflix.com>
2018-06-05 11:12:30 -0700

    autopep8 vscode.py

--- caster/apps/vscode.py
+++ caster/apps/vscode.py
@@ -1,4 +1,5 @@
-from dragonfly import (Grammar, AppContext, Dictation, Key, Text, Repeat, Function)
+from dragonfly import (Grammar, AppContext, Dictation,
+                       Key, Text, Repeat, Function)
 
 from caster.lib import control
 from caster.lib import settings
@@ -6,6 +7,7 @@ from caster.lib.dfplus.additions import IntegerRefST
 from caster.lib.dfplus.merge import gfilter
 from caster.lib.dfplus.merge.mergerule import MergeRule
 from caster.lib.dfplus.state.short import R
+
 
 def select_lines(x, y):
     '''Selects line x through y inclusive.'''
@@ -20,17 +22,20 @@ def select_lines(x, y):
     Text("%(n)d").execute({"n": x})
     Key("enter").execute()
     Key("c-i").execute()  # Selects entire line
-    Key("s-down:%(n)d").execute({"n": y - x})  #Select y-x following lines
-    
+    Key("s-down:%(n)d").execute({"n": y - x})  # Select y-x following lines
+
+
 def delete_lines(x, y):
     '''Deletes between line x and the line y inclusive.'''
     select_lines(x, y)
     Key("del").execute()
 
+
 def copy_lines(x, y):
     '''Copy content from line x through y, inclusive.'''
     select_lines(x, y)
     Key("c-c").execute()
+
 
 def cut_lines(x, y):
     '''Cut content from line x through y, inclusive.'''
@@ -47,6 +52,7 @@ def find_nth_token(text, n, direction):
         Key("enter:%(n)d").execute({"n": n})
         Key('escape').execute()
 
+
 class VisualStudioCodeRule(MergeRule):
     pronunciation = "visual studio code"
 
@@ -58,15 +64,16 @@ class VisualStudioCodeRule(MergeRule):
         "Close (tab|file)": R(Key("c-w"), rdescript="Visual Studio Code: Close Tab"),
         "Save file": R(Key("c-s"), rdescript="Visual Studio Code: Save File"),
         "Save and close": R(Key("c-s/10, c-w"), rdescript="Visual Studio Code: Save And Close File"),
-       
+
         # Search
         "(search | find in) [all] (files | codebase)": R(Key("cs-f"), rdescript="Visual Studio Code: Find in Codebase"),
         "(search | find) [file]": R(Key("c-f"), rdescript="Visual Studio Code: Find in File"),
         "(Find | Jump [to]) next <text>": R(Function(find_nth_token, n=1, direction="forward"), rdescript="Visual Studio Code: Find Next"),
         "(Find | Jump [to]) previous <text>": R(Function(find_nth_token, n=1, direction="reverse"), rdescript="Visual Studio Code: Find Previous"),
 
-        # Tab management       
-        "nexta [<n>]": R(Key("c-pgdown"), rdescript="Visual Studio Code: Next Tab") * Repeat(extra="n"),  # These would be next and previous tab but i have a conflict with chrome
+        # Tab management
+        # These would be next and previous tab but i have a conflict with chrome
+        "nexta [<n>]": R(Key("c-pgdown"), rdescript="Visual Studio Code: Next Tab") * Repeat(extra="n"),
         "prexta [<n>]": R(Key("c-pgup"), rdescript="Visual Studio Code: Previous Tab") * Repeat(extra="n"),
         "Close tab": R(Key("c-f4"), rdescript="Visual Studio Code: Close Tab"),
         "Exit preview": R(Key("space, c-z"), rdescript="Visual Studio Code: Exit Preview"),
@@ -87,10 +94,10 @@ class VisualStudioCodeRule(MergeRule):
         "Unindent [<n>]": R(Key("s-tab"), rdescript="Visual Studio Code: Unindent") * Repeat(extra="n"),
         "Comment": R(Key("c-slash"), rdescript="Visual Studio Code: Line Comment"),
         "Block comment": R(Key("sa-a"), rdescript="Visual Studio Code: Block Comment"),
-        "format document": R(Key("sa-f"),rdesript = "Format the entire document"),
-        "format selection": R(Key("sa-f"),rdesript = "Format only the selected lines"),
+        "format document": R(Key("sa-f"), rdesript="Format the entire document"),
+        "format selection": R(Key("sa-f"), rdesript="Format only the selected lines"),
 
-        # Editing 
+        # Editing
         "select (line|lines) <x> [through <y>]": R(Function(select_lines), rdescript="VSC: Select lines"),
         "copy (lines|line) <x> [through <y>]":   R(Function(copy_lines), rdescript="VSC: Copy lines"),
         "cut (lines|line) <x> [through <y>]":    R(Function(cut_lines), rdescript="VSC: Cut lines"),
@@ -99,7 +106,7 @@ class VisualStudioCodeRule(MergeRule):
         "shrink selection [<n> [(times|time)]]": R(Key("sa-left"), rdescript="VSC: shrink selection") * Repeat(extra="n"),
 
         # Window Management
-        "[toggle] full screen":         R(Key("f11"), rdescript="Visual Studio Code:Fullscreen"),        
+        "[toggle] full screen":         R(Key("f11"), rdescript="Visual Studio Code:Fullscreen"),
         "[toggle] Zen mode":            R(Key("c-k/3, z")),
 
         # Debugging
@@ -108,23 +115,24 @@ class VisualStudioCodeRule(MergeRule):
         "step into":                    R(Key("f11"), rdescript="Visual Studio Code:Step Into"),
         "step out [of]":                R(Key("s-f11"), rdescript="Visual Studio Code:Step Out"),
         "resume":                       R(Key("f5"), rdescript="Visual Studio Code:Resume"),
-                }
+    }
     extras = [
-              Dictation("text"),
-              Dictation("mim"),
-              IntegerRefST("n", 1, 1000),
-              IntegerRefST("x", -1, 10000),
-              IntegerRefST("y", -1, 10000)
-             ]
+        Dictation("text"),
+        Dictation("mim"),
+        IntegerRefST("n", 1, 1000),
+        IntegerRefST("x", -1, 10000),
+        IntegerRefST("y", -1, 10000)
+    ]
     defaults = {
         "n": 1,
         "x": -1,
         "y": -1,
         "mim": "",
         "text": ""
-        }
+    }
 
 # ---------------------------------------------------------------------------
+
 
 context = AppContext(executable="code")
 grammar = Grammar("Visual Studio Code", context=context)
