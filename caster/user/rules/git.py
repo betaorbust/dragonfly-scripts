48a16c71682e32691d2ea300dba5f092cb3ce292
Jacques Favreau <Jacques@lgml-jfavreau.corp.netflix.com>
2018-06-05 10:51:52 -0700

    add a whole bunch of git commands

--- caster/user/rules/git.py
+++ caster/user/rules/git.py
@@ -13,18 +13,28 @@ def tab_if_text(text):
 class Git(MergeRule):
     pronunciation = "Git"
     mapping = {
+        "(git|get) branch":                 R(Text("git branch") + Key("enter"), rdescript="git branch"),
         "(git|get) status":                 R(Text("git status") + Key("enter"), rdescript="git status"),
+        "(git|get) diff":                   R(Text("git diff"), rdescript="git diff"),
         "(git|get) add":                    R(Text("git add -A"), rdescript="git add"),
         "(git|get) commit":                 R(Text("git commit -m \"\"") + Key("left"), rdescript="git commit"),
         "(git|get) commit (amend|and)":     R(Text("git commit --amend"), rdescript="git commit amend"),
+        "(git|get) push [<push_flag>] [<push_flag_2>]": R(Text("git push %(push_flag)s %(push_flag_2)s"), rdescript="git push"),
         "(git|get) pull [<pull_flag>]":     R(Text("git pull %(pull_flag)s"), rdescript="git pull"),
-        "(git|get) stash":                  R(Text("git stash"), rdescript="git stash"),
+        "(git|get) stash [<text>]":         R(Text("git stash save \"%(text)s\"") + Key("left"), rdescript="git stash"),
         "(git|get) stash pop":              R(Text("git stash pop"), rdescript="git stash pop"),
         "(git|get) new branch [<text>]":    R(Text("git checkout -b %(text)s"), rdescript="git new branch"),
         "(git|get) check out [<text>]":     R(Text("git checkout %(text)s") + Function(tab_if_text), rdescript="git checkout"),
         "(git|get) rebase [<rebase_flag>]": R(Text("git rebase %(rebase_flag)s"), rdescript="git rebase"),
+        "(git|get) reset":                  R(Text("git reset "), rdescript="git reset"),
     }
     
+
+    gitPushMap = {
+        "force": "--force",
+        "no verify": "--no-verify"
+    }
+
     extras   = [
         IntegerRefST("n", 1, 50),
         Dictation("text"),
@@ -34,6 +44,8 @@ class Git(MergeRule):
         Choice("pull_flag", {
             "rebase": "--rebase "
         }),
+        Choice("push_flag", gitPushMap),
+        Choice("push_flag_2", gitPushMap)
     ]
 
     defaults = {
@@ -41,6 +53,8 @@ class Git(MergeRule):
         "text": "",
         "rebase_flag": "",
         "pull_flag": "",
+        "push_flag": "",
+        "push_flag_2": "" 
     }
     
 control.nexus().merger.add_global_rule(Git())
